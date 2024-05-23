3e2118658392f76e3b58ba2348b82343a4c48565
bagashanantoputra <danandjaya123@gmail.com>
2024-05-23 22:26:28 +0700

    finishing code

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,36 +1,57 @@
 import HeaderBox from '@/components/HeaderBox'
+import RecentTransactions from '@/components/RecentTransactions';
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 import { getLoggedInUser } from '@/lib/actions/user.action';
 
-const Home = async () => {
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ 
+    userId: loggedIn.$id 
+  })
+
+  if(!accounts) return;
   
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId })
+
+  console.log(accountsData, account)
+
   return (
-    <section className='home'>
-        <div className='home-content'>
-          <header className='home-header'>
-            <HeaderBox 
-              type = 'greeting'
-              title = 'Welcome'
-              user = {loggedIn?.name || 'Guest'}
-              subtext = 'Access and manage your account and transactions efficiently'
-            />
-            <TotalBalanceBox 
-              accounts={[]}
-              totalBanks={1}
-              totalCurrentBalance={1250.00}
-            />
-          </header>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
 
-        RECENT TRANSACTIONS
-        </div>
+          <TotalBalanceBox 
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
+          />
+        </header>
 
-        <RightSidebar 
-          user={loggedIn}
-          transactions={[]}
-          banks={[{currentBalance: 120.50},{currentBalance: 423.50}]}
+        <RecentTransactions 
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
         />
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      />
     </section>
   )
 }
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,8 +1,9 @@
-'use client'
+'use client';
 
-import React, { useState } from 'react'
-import Link from 'next/link'
 import Image from 'next/image'
+import Link from 'next/link'
+import React, { useState } from 'react'
+
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -16,45 +17,61 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
-import { authFormSchema } from '@/lib/utils'
-import CustomInput from './CustomInput'
+import CustomInput from './CustomInput';
+import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
-import { useRouter } from 'next/navigation'
-import { signIn, signUp } from '@/lib/actions/user.action'
+import { useRouter } from 'next/navigation';
+import PlaidLink from './PlaidLink';
+import { signIn, signUp } from '@/lib/actions/user.action';
 
-const AuthForm = ({type}: {type: string}) => {
+const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   const formSchema = authFormSchema(type);
-  
+
     // 1. Define your form.
     const form = useForm<z.infer<typeof formSchema>>({
       resolver: zodResolver(formSchema),
       defaultValues: {
         email: "",
-        password: ""
+        password: ''
       },
     })
-
+   
+    // 2. Define a submit handler.
     const onSubmit = async (data: z.infer<typeof formSchema>) => {
       setIsLoading(true);
+
       try {
         // Sign up with Appwrite & create plaid token
         
         if(type === 'sign-up') {
-          const newUser = await signUp(data);
+          const userData = {
+            firstName: data.firstName!,
+            lastName: data.lastName!,
+            address1: data.address1!,
+            city: data.city!,
+            state: data.state!,
+            postalCode: data.postalCode!,
+            dateOfBirth: data.dateOfBirth!,
+            ssn: data.ssn!,
+            email: data.email,
+            password: data.password
+          }
+
+          const newUser = await signUp(userData);
 
           setUser(newUser);
         }
 
         if(type === 'sign-in') {
           const response = await signIn({
-              email: data.email,
-              password: data.password,
+            email: data.email,
+            password: data.password,
           })
-        
+
           if(response) router.push('/')
         }
       } catch (error) {
@@ -65,7 +82,7 @@ const AuthForm = ({type}: {type: string}) => {
     }
 
   return (
-    <section className='auth-form'>
+    <section className="auth-form">
       <header className='flex flex-col gap-5 md:gap-8'>
           <Link href="/" className="cursor-pointer flex items-center gap-1">
             <Image 
@@ -76,6 +93,7 @@ const AuthForm = ({type}: {type: string}) => {
             />
             <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Horizon</h1>
           </Link>
+
           <div className="flex flex-col gap-1 md:gap-3">
             <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
               {user 
@@ -95,13 +113,13 @@ const AuthForm = ({type}: {type: string}) => {
       </header>
       {user ? (
         <div className="flex flex-col gap-4">
-          {/* <PlaidLink user={user} variant="primary" /> */}
+          <PlaidLink user={user} variant="primary" />
         </div>
       ): (
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              { type === 'sign-up' && (
+              {type === 'sign-up' && (
                 <>
                   <div className="flex gap-4">
                     <CustomInput control={form.control} name='firstName' label="First Name" placeholder='Enter your first name' />
@@ -121,6 +139,7 @@ const AuthForm = ({type}: {type: string}) => {
               )}
 
               <CustomInput control={form.control} name='email' label="Email" placeholder='Enter your email' />
+
               <CustomInput control={form.control} name='password' label="Password" placeholder='Enter your password' />
 
               <div className="flex flex-col gap-4">
